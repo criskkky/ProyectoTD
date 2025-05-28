51ecf9f99ffe827346d24c1fd1ae11b11785e262
cristóbal <criskywww@gmail.com>
2025-05-28 17:04:18 +0000

    feat(front): main routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from '@pages/Login';
+import Home from '@pages/Home';
+import Users from '@pages/Users';
+import Register from '@pages/Register';
+import Error404 from '@pages/Error404';
+import Root from '@pages/Root';
+import ProtectedRoute from '@components/ProtectedRoute';
+import '@styles/styles.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <Error404 />,
+    children: [
+      {
+        path: '/home',
+        element: <Home />
+      },
+      {
+        path: '/users',
+        element: (
+          <ProtectedRoute allowedRoles={['admin']}>
+            <Users />
+          </ProtectedRoute>
+        ),
+      }
+    ],
+  },
+  {
+    path: '/auth',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <Register />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
