a2ec32e36e9c9b0d5edb8dd08695640aa95e92ad
criskkky <criskywww@gmail.com>
2025-05-29 23:16:50 -0400

    feat(front): add sweetalert

---
+++ frontend/src/helpers/sweetAlert.js
@@ -0,0 +1,37 @@
+import Swal from 'sweetalert2';
+
+export async function deleteDataAlert() {
+  return Swal.fire({
+    title: "¿Estás seguro?",
+    text: "¡No podrás revertir esto!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si, eliminar!"
+  })
+}
+
+export const showSuccessAlert = (titleMessage, message) => {
+  Swal.fire(
+    titleMessage,
+    message,
+    'success'
+  );
+};
+
+export const showErrorAlert = (titleMessage, message) => {
+  Swal.fire(
+    titleMessage,
+    message,
+    'error'
+  );
+};
+
+export const showWarningAlert = (titleMessage, message) => {
+  Swal.fire(
+    titleMessage,
+    message,
+    'warning'
+  );
+}
