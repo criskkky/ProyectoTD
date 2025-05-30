e9d11ac127539167acfaa99bf10d63997128176d
criskkky <criskywww@gmail.com>
2025-05-29 22:09:46 -0400

    feat(front): initial structure

---
+++ frontend/src/helpers/formatData.js
@@ -0,0 +1,34 @@
+import { startCase } from 'lodash';
+import { format as formatRut } from 'rut.js';
+import { format as formatTempo } from "@formkit/tempo";
+
+export function formatUserData(user) {
+    return {
+        ...user,
+        nombres: startCase(user.nombres),
+        apellidos: startCase(user.apellidos),
+        rol: startCase(user.rol),
+        rut: formatRut(user.rut),
+        createdAt: formatTempo(user.createdAt, "DD-MM-YYYY")
+    };
+}
+
+export function convertirMinusculas(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'string') {
+            obj[key] = obj[key].toLowerCase();
+        }
+    }
+    return obj;
+}
+
+export function formatPostUpdate(user) {
+    return {
+        nombres: startCase(user.nombres),
+        apellidos: startCase(user.apellidos),
+        rol: startCase(user.rol),
+        rut: formatRut(user.rut, { dots: false }),
+        email: user.email,
+        createdAt: formatTempo(user.createdAt, "DD-MM-YYYY")
+    };
+}
---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,52 @@
+import axios from './root.service.js';
+import cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
+import { convertirMinusculas } from '@helpers/formatData.js';
+
+export async function login(dataUser) {
+    try {
+        const response = await axios.post('/auth/login', {
+            email: dataUser.email, 
+            password: dataUser.password
+        });
+        const { status, data } = response;
+        if (status === 200) {
+            const { nombres, apellidos, email, rut, rol } = jwtDecode(data.data.token);
+            const userData = { nombres, apellidos, email, rut, rol };
+            sessionStorage.setItem('usuario', JSON.stringify(userData));
+            axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+            cookies.set('jwt-auth', data.data.token, {path:'/'});
+            return response.data
+        }
+    } catch (error) {
+        return error.response.data;
+    }
+}
+
+export async function register(data) {
+    try {
+        const dataRegister = convertirMinusculas(data);
+        const { nombres, apellidos, email, rut, password } = dataRegister
+        const response = await axios.post('/auth/register', {
+            nombres,
+            apellidos,
+            email,
+            rut,
+            password
+        });
+        return response.data;
+    } catch (error) {
+        return error.response.data;
+    }
+}
+
+export async function logout() {
+    try {
+        await axios.post('/auth/logout');
+        sessionStorage.removeItem('usuario');
+        cookies.remove('jwt');
+        cookies.remove('jwt-auth');
+    } catch (error) {
+        console.error('Error al cerrar sesión:', error);
+    }
+}
