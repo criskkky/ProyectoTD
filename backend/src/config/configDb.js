84fad0d3ee7c05d52addc8962da834427b625c3f
criskkky <criskywww@gmail.com>
2025-05-30 20:28:02 -0400

    fix(front): api port
    Revert "fix(front): api port"
    
    This reverts commit b8e55988be47e001fbf897df185abe72d749dc10.
    
    fix(back): update database port configuration

--- backend/src/config/configDb.js
+++ backend/src/config/configDb.js
@@ -1,14 +1,14 @@
 "use strict";
 import { DataSource } from "typeorm";
-import { DB_NAME, DB_USER, HOST, DB_PASSWORD, PORT } from "./configEnv.js";
+import { DB_NAME, DB_USER, HOST, DB_PASSWORD, DB_PORT } from "./configEnv.js";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: `${HOST}`,
-  port: `${PORT} || 3306`,
-  username: `${DB_USER}`,
-  password: `${DB_PASSWORD}`,
-  database: `${DB_NAME}`,
+  host: HOST,
+  port: DB_PORT,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
   entities: ["src/entity/**/*.js"],
   synchronize: true,
   logging: false, // SQL logs
--- backend/src/config/configEnv.js
+++ backend/src/config/configEnv.js
@@ -16,5 +16,6 @@ export const PORT = process.env.PORT;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_NAME = process.env.DB_NAME;
+export const DB_PORT = process.env.DB_PORT;
 export const TOKEN_JWT = process.env.TOKEN_JWT;
 export const cookieKey = process.env.cookieKey;
