fa9ba903a09f9afc679e55e70f8e35382f31a3bd
cristóbal <criskywww@gmail.com>
2025-07-30 17:44:53 -0400

    feat(Root): implement ScrollToTop component for better navigation experience

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,24 +1,35 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { AuthProvider } from '@/context/AuthContext';
+import { useEffect } from 'react';
+
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function Root()  {
-return (
+  return (
     <AuthProvider>
-        <PageRoot/>
+      <ScrollToTop />
+      <PageRoot/>
     </AuthProvider>
-);
+  );
 }
 
 function PageRoot() {
-return (
+  return (
     <>
-        <Navbar />
-        <Outlet />
-        <Footer />
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
-);
+  );
 }
 
 export default Root;
